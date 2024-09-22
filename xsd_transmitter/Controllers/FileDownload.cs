4079e2ac4a92595809e82216d6e11ddcb8abc3e1
Белоусов Юрий Андреевич <belousovyua@melonfashion.com>
2024-09-22 16:23:15 +0500

    init

---
+++ xsd_transmitter/Controllers/FileDownload.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+namespace xsd_transmitter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileDownload : Controller
+    {
+        [HttpGet("download")]
+        public IActionResult DownloadFile()
+        {
+            // Путь к файлу (можно задать полный путь или относительный к корню проекта)
+            var filePath = "files/goods-catalog-schema.xsd";
+
+            // Проверяем, существует ли файл
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            // Читаем файл в байты
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            // Возвращаем файл с соответствующим контент-тайпом и заголовком для загрузки
+            return File(fileBytes, "application/octet-stream", Path.GetFileName(filePath));
+        }
+    }
+}
---
+++ xsd_transmitter/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace xsd_transmitter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.Urls.Add("http://0.0.0.0:5000");
+
+            app.Run();
+        }
+
+        
+    }
+
+}
